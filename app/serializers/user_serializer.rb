93d118454de9fe05b53c56c7a79f4ff83bd985dd
jkrouse13 <jon.krouse@gmail.com>
2017-11-10 15:27:25 -0500

    testing123

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes  :name, :email, :api_token, :file, :id, :followers_count, :followees_count
+  attributes  :name, :email, :api_token, :file, :id, :followers_count, :followees_count, :route_to_file
 
-
+  def route_to_file
+    'https://www.herokuapp.heroku.com' + file
+  end
 
   def file
      Refile.attachment_url(object, :file, :fit, 50, 50, format: "jpg")
